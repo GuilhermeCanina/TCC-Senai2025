f9c0d994aa1a87921aca9d808c83334b3fecf045
GuilhermeCanina <caninaguilherme33@gmail.com>
2025-04-12 20:13:24 -0300

    criando o server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+const PORTA = 3001;
+
+app.listen(PORTA, () => {
+  console.log(`Servidor rodando na porta ${PORTA}`);
+});
+
+module.exports = routes;
