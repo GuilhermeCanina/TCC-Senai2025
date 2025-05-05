e037ce90cc9ce265eaa571e63105d50ac17b6eb6
GuiMassucatto <guimassucatto@outlook.com>
2025-05-05 16:46:19 -0300

    criação de controllerMedalhas

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const routes = express.Router();
 
 const usuariosController = require('./controllers/controllerUser');
+const medalhasController = require('./controllers/controllerMedalha');
 
 // Definindo as rotas
 
 routes.get('/usuarios', usuariosController.getAllUsers);
 routes.get('/usuarios/:id', usuariosController.getUserById);
 routes.post('/usuarios', usuariosController.createUser);
+
+routes.get('/medalhas', medalhasController.getAllMedalhas);
+routes.get('/medalhas/:id', medalhasController.getMedalhaById);
+routes.post('/medalhas', medalhasController.createMedalha);
 
 
 module.exports = routes;
