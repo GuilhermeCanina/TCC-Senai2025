390683261db56a2365c58ee07ea854e646e0dc01
Guilherme Canina <161065800+GuilhermeCanina@users.noreply.github.com>
2025-10-06 13:45:19 -0300

    Delete api directory

--- api/src/iaRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { chatIA } from "./controllers/controllermensagens.js";
-
-const router = express.Router();
-
-router.post("/chat", chatIA);
-
-export default router;
--- api/src/middlewares/isAdmin.js
+++
@@ -1,9 +0,0 @@
-function isAdmin(req, res, next) {
-  if (req.user && req.user.role === 'admin') {
-    return next();
-  }
-
-  return res.status(403).json({ error: 'Acesso negado. Requer privilégios de administrador.' });
-}
-
-module.exports = isAdmin;
